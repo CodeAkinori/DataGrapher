de983a2aeb7cb945f0c13dbc2531ac822d8ced89
AkinoriKoerich <yuukko404@gmail.com>
2023-07-16 06:00:34 -0300

    Initial Commit

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template, request
-
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        data = request.form.get('numbers')
+        numbers = [int(num.strip()) for num in data.split(',')]
+        
+        # Gerar o gráfico de pizza
+        plt.pie(numbers, labels=numbers, autopct='%1.1f%%')
+        plt.axis('equal')
+        plt.title('Gráfico de Pizza')
+        plt.savefig('static/pie_chart.png')
+        plt.close()
+        
+        return render_template('chart.html')
+    
     return render_template('index.html')
 
-@app.route('/graphpizza', methods=['GET', 'POST'])
-def graph_pizza():
-    if request.method == 'POST':
-        quantidade = int(request.form['quantidade'])
-        return render_template('graph_pizza.html', quantidade=quantidade)
-    return render_template('graph_pizza.html')
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
