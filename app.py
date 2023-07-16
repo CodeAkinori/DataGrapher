a7e4da8318894eeac2169956a3f09b90590b4a3a
AkinoriKoerich <yuukko404@gmail.com>
2023-07-16 06:09:05 -0300

    Update pizza!

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, request
 import matplotlib.pyplot as plt
+import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/create_chart', methods=['GET', 'POST'])
+def create_chart():
     if request.method == 'POST':
         data = request.form.get('numbers')
-        numbers = [int(num.strip()) for num in data.split(',')]
-        
-        # Gerar o gráfico de pizza
-        plt.pie(numbers, labels=numbers, autopct='%1.1f%%')
-        plt.axis('equal')
-        plt.title('Gráfico de Pizza')
-        plt.savefig('static/pie_chart.png')
-        plt.close()
-        
-        return render_template('chart.html')
+        if data:
+            numbers = [int(num.strip()) for num in data.split(',')]
+            
+            # Gerar o gráfico de pizza
+            plt.pie(numbers, labels=numbers, autopct='%1.1f%%')
+            plt.axis('equal')
+            plt.title('Gráfico de Pizza')
+            
+            # Salvar o gráfico em um arquivo
+            chart_path = os.path.join('static', 'pie_chart.png')
+            plt.savefig(chart_path)
+            plt.close()
+            
+            return redirect(url_for('chart'))
     
-    return render_template('index.html')
+    return render_template('create_chart.html')
+
+
+@app.route('/chart')
+def chart():
+    return render_template('chart.html')
 
 if __name__ == '__main__':
     app.run()
