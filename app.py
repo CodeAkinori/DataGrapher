83a9bc37615d6054a66f6703b2e2a7cdd2ad0f00
AkinoriKoerich <yuukko404@gmail.com>
2023-07-17 13:22:28 -0300

    Bar Chart Update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 import matplotlib.pyplot as plt
+
 import os
 
 app = Flask(__name__)
@@ -27,12 +28,39 @@ def create_chart():
             
             return redirect(url_for('chart'))
     
-    return render_template('create_chart.html')
+    return render_template('piechart/create_chart.html')
 
 
 @app.route('/chart')
 def chart():
-    return render_template('chart.html')
+    return render_template('piechart/chart.html')
+
+@app.route('/chart_bar')
+def chart_bar():
+    return render_template('barchart/chart_bar.html')
+
+
+@app.route('/create_bar_chart', methods=['GET', 'POST'])
+def create_bar_chart():
+    if request.method == 'POST':
+        data = request.form.get('numbers')
+        if data:
+            numbers = [int(num.strip()) for num in data.split(',')]
+
+            # Gerar o gráfico de barras
+            plt.bar(range(len(numbers)), numbers)
+            plt.xlabel('Categoria')
+            plt.ylabel('Valor')
+            plt.title('Bar Chart')
+
+            # Salvar o gráfico em um arquivo, para exibição
+            chart_path = os.path.join('static', 'img', 'bar_chart.png')
+            plt.savefig(chart_path)
+            plt.close()
+
+            return redirect(url_for('chart_bar'))
+
+    return render_template('barchart/create_bar_chart.html')
 
 
 if __name__ == '__main__':
